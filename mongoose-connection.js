8922880c2d78982199b5aa9b69e9873d1f5052b5
Anish007TML <157500849+Anish007TML@users.noreply.github.com>
2025-08-15 00:38:31 +0530

    Create mongoose-connection.js

---
+++ mongoose-connection.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const config = require("config");
+
+const dbgr = require("debug")("development:mongoose");
+
+
+mongoose
+  .connect(`${config.get("MONGODB_URI")}/scatch`)
+  .then(() => {
+    dbgr("Connected");
+  })
+  .catch((err) => {
+    console.log("Connection error:", err);
+  });
+
+module.exports = mongoose.connection;
